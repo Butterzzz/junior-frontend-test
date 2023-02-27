a6e167307e784ff0a5f960e5d14aacd12bd6b6c1
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-27 15:51:25 +0900

    feat: add basic structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from './components/Header/Header'
+import Filters from './components/Filters/Filters'
 import ProductList from './components/ProductList/ProductList'
 
 function App() {
   return (
     <div>
+      <Header />
+      <Filters />
       <ProductList />
     </div>
   )
