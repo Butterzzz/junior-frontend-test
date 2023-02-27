4a6eed75aafa15db7259c35f03e5775a8c645edd
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-27 16:22:03 +0900

    feat: add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h2 className="header__title">Products</h2>
+        <div className="header__search">
+          <span className="header__search-icon"></span>
+          <input className="header__search-input" type="text" placeholder="Search among products" />
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
